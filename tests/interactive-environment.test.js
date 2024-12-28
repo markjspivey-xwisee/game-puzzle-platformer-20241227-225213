f76477126cdc266ed08df6fe0a546656f2ddef21
markjspivey-xwisee <114436194+markjspivey-xwisee@users.noreply.github.com>
2024-12-27 22:53:33 -0500

    Implement [HIGH] Interactive environment
    Closes #3

---
+++ tests/interactive-environment.test.js
@@ -0,0 +1,8 @@
+
+                import { expect } from 'chai';
+                import * as feature from '../src/features/interactive-environment';
+                
+                describe('interactive-environment', () => {
+                    // Add tests here
+                });
+                
